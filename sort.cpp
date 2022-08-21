0ed3e59a6f4102952f394be9fe63878fc0720d9f
seiya <seiya.111122@gmail.com>
2022-08-21 18:30:11 +0900

    add selectionsort and insersionsort

--- sort.cpp
+++ sort.cpp
@@ -3,15 +3,30 @@ using namespace std;
 #define rep(i,i0,n) for (int i =(int)(i0); i < (int)(n); i++)
 using ll=long long;
 
-void bubblesort(vector<int> &vec){
+void BubbleSort(vector<int> &vec){
     int lenV = vec.size();
     rep(i, 0, lenV){
         rep(j, i+1, lenV){
-            if (vec[i]>vec[j]){
-                int smaller = vec[j];
-                vec[j] = vec[i];
-                vec[i] = smaller;
-            }
+            if (vec[i]>vec[j]) swap(vec[i], vec[j]);
+        }
+    }
+}
+
+void SelectionSort(vector<int> &vec){
+    int lenV = vec.size();
+    rep(i, 0, lenV-1){
+        auto min_element_itr = *min_element(vec.begin()+i, vec.end());
+        swap(vec[i], *min_element_itr);
+    }
+}
+
+void InsersionSort(vector<int> &vec){
+    int lenV = vec.size();
+    rep(i, 1, lenV){
+        int pos = i;
+        while (pos && vec[pos-1]>vec[pos]){
+            swap(vec[pos-1], vec[pos]);
+            pos--;
         }
     }
 }
