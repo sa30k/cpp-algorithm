ab82b24ed97865e718d8cfe4d5ff0393e853bb5d
seiya <seiya.111122@gmail.com>
2022-08-23 02:06:09 +0900

    add quicksort and randomquicksort

--- sort.cpp
+++ sort.cpp
@@ -3,6 +3,9 @@ using namespace std;
 #define rep(i,i0,n) for (int i =(int)(i0); i < (int)(n); i++)
 using ll=long long;
 
+mt19937 mt;
+random_device rnd;
+
 void BubbleSort(vector<int> &vec){
     int lenV = vec.size();
     rep(i, 0, lenV){
@@ -29,4 +32,48 @@ void InsersionSort(vector<int> &vec){
             pos--;
         }
     }
+}
+
+void QuickSort(vector<int> &vec) {
+    if (!vec.size()) return;
+
+    int lenV = vec.size();
+    int x = lenV/2;
+    vector<int> l, r;
+    rep(i, 0, lenV){
+        if (i==x) continue;
+        if (vec[i]<vec[x]) l.push_back(vec[i]);
+        else r.push_back(vec[i]);
+    }
+
+    QuickSort(l);
+    QuickSort(r);
+
+    l.push_back(vec[x]);
+    l.insert(l.end(), r.begin(), r.end());
+    vec = l;
+}
+
+void RandomQuickSort(vector<int> &vec) {
+    if (!vec.size()) return;
+
+    int lenV = vec.size();
+    int x = mt() % lenV;
+    vector<int> l, r;
+    rep(i, 0, lenV){
+        if (i==x) continue;
+        if (vec[i]==vec[x]){
+            if (mt()%2) l.push_back(vec[i]);
+            else r.push_back(vec[i]);
+        }
+        else if (vec[i]<vec[x]) l.push_back(vec[i]);
+        else r.push_back(vec[i]);
+    }
+
+    RandomQuickSort(l);
+    RandomQuickSort(r);
+
+    l.push_back(vec[x]);
+    l.insert(l.end(), r.begin(), r.end());
+    vec = l;
 }
